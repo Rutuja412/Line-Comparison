acbed391c18c6ea296b5952656c8b0d0ec1d2bb9
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-10 09:16:48 +0530

    [Rutuja]:Check Equality of lines

--- Line-Comparison/LenghtOfLine.cs
+++ Line-Comparison/LenghtOfLine.cs
@@ -17,8 +17,9 @@ namespace Line_Comparison
             Console.WriteLine("Enter the line point y2: ");
             int y2 = Convert.ToInt32(Console.ReadLine());
             var Length1 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-            Console.WriteLine("Length1 is:" + Length1);
-            Console.WriteLine("Enter the line point x3: ");
+             Console.WriteLine("Length1 is:" + Length1);
+
+            Console.WriteLine(" Enter the line point x3: ");
             int x3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the line point x4: ");
             int x4 = Convert.ToInt32(Console.ReadLine());
@@ -27,7 +28,16 @@ namespace Line_Comparison
             Console.WriteLine("Enter the line point y4: ");
             int y4 = Convert.ToInt32(Console.ReadLine());
             var Length2 = Math.Sqrt((Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2)));
-            Console.WriteLine("Length2 is:" + Length2);
+            Console.WriteLine("Length2 is: " + Length2);
+
+            if (Length1.Equals(Length2))
+            {
+                Console.WriteLine("The two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("The two lines are not equal");
+            }
         }
     }
 }
